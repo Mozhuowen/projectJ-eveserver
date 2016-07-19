783ed5309b849374e4018776340ac014d16e0f17
莫卓文 <mozhuowen@mozhuowendeMacBook-Pro.local>
2016-07-19 16:37:53 +0800

    webhook-test

--- util/str_util.py
+++ util/str_util.py
@@ -4,5 +4,5 @@ import random
 
 
 def get_random_string(len):
-    a = 'webhook-test'
+    a = 'webhook-test-test'
     return ''.join(random.sample('0123456789zyxwvutsrqponmlkjihgfedcbaQWERTYUIOPASDFGHJKLZXCVBNM', len))
